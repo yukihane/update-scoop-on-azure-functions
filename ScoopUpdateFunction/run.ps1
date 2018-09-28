03d243407f3a192738cfff3fa7be3c493f2b3f8c
yukihane <yukihane.feather@gmail.com>
2018-09-29 01:31:46 +0900

    try git execution

--- ScoopUpdateFunction/run.ps1
+++ ScoopUpdateFunction/run.ps1
@@ -2,3 +2,5 @@ $approot="${env:HOME}/site/wwwroot/ScoopUpdateFunction"
 
 Write-Output "${env:Home}"
 Write-Output "PowerShell Timer trigger function executed at:$(get-date)";
+
+iex "$approot/bin/git/cmd/git.exe --version"

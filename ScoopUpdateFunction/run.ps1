916303f6c1444beaf481d3352534cf347746e5a3
yukihane <yukihane.feather@gmail.com>
2018-09-29 06:59:32 +0900

    一旦完成？

--- ScoopUpdateFunction/run.ps1
+++ ScoopUpdateFunction/run.ps1
@@ -1,6 +1,41 @@
-$approot="${env:HOME}/site/wwwroot/ScoopUpdateFunction"
+# $approot="${env:HOME}/site/wwwroot/ScoopUpdateFunction"
+$approot = $EXECUTION_CONTEXT_FUNCTIONDIRECTORY
+$tempdir = "${env:TEMP}"
 
-Write-Output "${env:Home}"
-Write-Output "PowerShell Timer trigger function executed at:$(get-date)";
+$bucket = "scoop-bucket-yukihane-games"
+$bucketurl = "git@github.com:yukihane/$bucket.git"
 
-iex "$approot/bin/git/cmd/git.exe --version"
+# cacheディレクトリ決定のために必要
+Set-Item env:SCOOP "$tempdir/tmp_scoop"
+# scoop本体があるディレクトリ
+$scooproot = "$approot/bin/scoop"
+
+$git = "$approot/bin/git/cmd/git.exe"
+
+Set-Location "$tempdir"
+Invoke-Expression $git clone $bucketurl
+$bucketdir = "$tempdir/$bucket"
+Set-Location "$bucketdir"
+
+
+
+. "$scooproot/lib/manifest.ps1"
+
+$checkver = "$scooproot/bin/checkver.ps1"
+
+$dir = "."
+
+
+Get-ChildItem $dir -Filter "*.json"  | ForEach-Object {
+
+    $app = $_.BaseName
+    Invoke-Expression -Command "$checkver -app $app -dir $dir -update true"
+
+    $file = "$_.FullName"
+    $json = parse_json $file
+    $version = $json.version
+
+    Invoke-Expression $git commit $file -m "${app}: Update to version $version"
+}
+
+Invoke-Expression $git push
